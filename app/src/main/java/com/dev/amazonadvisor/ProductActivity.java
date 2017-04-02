631c1141a29d0942429b504e2dbd97f347ccbec6
Valerio Colombo <valecolomb96@gmail.com>
2017-04-03 00:59:06 +0200

    Add dropdown list in product_activity, new icon for fab

--- app/src/main/java/com/dev/amazonadvisor/ProductActivity.java
+++ app/src/main/java/com/dev/amazonadvisor/ProductActivity.java
@@ -7,23 +7,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.*;
+import android.widget.ExpandableListAdapter;
 
 import com.github.clans.fab.FloatingActionMenu;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by daniele on 28/03/2017.
  */
 
 public class ProductActivity extends AppCompatActivity {
-
+    private ExpandableListView listView;
+    private ExpandableListAdapter listAdapter;
+    private List<String> listDataHeader;
+    private HashMap<String, List<String>> listHash;
     //private FloatingActionMenu menuFab;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.product_activity);
+
+        listView = (ExpandableListView) findViewById(R.id.dropDownList);
+        initData();
+        listAdapter = new ExpandableList(this, listDataHeader, listHash);
+        listView.setAdapter(listAdapter);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.app_name);
         setSupportActionBar(toolbar);
@@ -33,5 +47,19 @@ public class ProductActivity extends AppCompatActivity {
         ((TextView)findViewById(R.id.product_title)).setText(getIntent().getStringExtra("Title"));
         /*menuFab = (FloatingActionMenu) findViewById(R.id.menu_yellow);
         menuFab.bringToFront();*/ //Should bring view to the top
+    }
+
+    private void initData() {
+        listDataHeader = new ArrayList<>();
+        listHash = new HashMap<>();
+
+        listDataHeader.add("Product details");
+
+        List<String> productDetails = new ArrayList<>();
+        productDetails.add("Seller");
+        productDetails.add("Prime");
+        productDetails.add("Estimated delivery");
+
+        listHash.put(listDataHeader.get(0), productDetails);
     }
 }
