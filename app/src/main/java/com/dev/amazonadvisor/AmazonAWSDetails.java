79fc3c5fc2d42ce81c747201319212dd3db004e0
Daniele Comi <superdani96@gmail.com>
2017-05-03 22:17:18 +0200

    Security first

--- app/src/main/java/com/dev/amazonadvisor/AmazonAWSDetails.java
+++ app/src/main/java/com/dev/amazonadvisor/AmazonAWSDetails.java
@@ -6,6 +6,6 @@ package com.dev.amazonadvisor;
 
 public interface AmazonAWSDetails {
 
-    String AWS_ACCESS_KEY_ID = "AKIAIIZGFFBWXZXJSQNA";
-    String AWS_SECRET_KEY = "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";
+    String AWS_ACCESS_KEY_ID = "AKIAIIZGFFBWXZXJSQNAXXX";
+    String AWS_SECRET_KEY = "gckgFtWVhJWbL9DeE0u5Pxha92TpItbDU+KWIwbSXXX";
 }
